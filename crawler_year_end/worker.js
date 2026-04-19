060468b042894a903d8a5467aebadd848501355c
Colin Truong Pascual <colinpascual@Colins-MacBook-Pro-5306.local>
2026-04-19 16:58:27 -0400

    fix

--- crawler_year_end/worker.js
+++ crawler_year_end/worker.js
@@ -4,7 +4,7 @@ const {storeYearEndState} = require('../storage/wikiStore.js');
 const util = require('util');
 
 const ORCHESTRATOR_SERVICE_NAME = 'orchestrator';
-const GID = 'wiki';
+const GID = 'wiki10';
 const YEARS = [
   2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
   2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
